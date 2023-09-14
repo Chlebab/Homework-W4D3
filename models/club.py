c9dab5b92d11dac53acdc13485a0135244533b92
Mikolaj <miksob@gmail.com>
2023-09-14 10:26:00 +0100

    added clubs name in display

--- models/club.py
+++ models/club.py
@@ -4,7 +4,7 @@ class Club(db.Model):
     __tablename__ = "clubs"
     id = db.Column(db.Integer, primary_key=True)
     club_name = db.Column(db.String(64))
-    players = db.relationship('Player', backref='player')
+    players = db.relationship('Player', backref='club')
     def __repr__(self):
         return f"<Club {self.id}: {self.club}>" 
     
